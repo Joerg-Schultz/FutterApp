ae348928c52f736f15c3db6aad1070607cc1650f
Joerg-Schultz <JoergSky@online.de>
2021-09-19 07:46:06 +0200

    insert Rating viewmodel test :-)

---
+++ app/src/androidTest/java/de/tierwohlteam/android/futterapp/viewModels/RatingViewModelTest.kt
@@ -0,0 +1,43 @@
+package de.tierwohlteam.android.futterapp.viewModels
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.test.filters.SmallTest
+import com.google.common.truth.Truth.assertThat
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import de.tierwohlteam.android.futterapp.getOrAwaitValueTest
+import de.tierwohlteam.android.futterapp.others.Status
+import de.tierwohlteam.android.futterapp.repositories.FutterAppRepository
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import javax.inject.Inject
+
+@ExperimentalCoroutinesApi
+@SmallTest
+@HiltAndroidTest
+class RatingViewModelTest {
+    @get:Rule
+    var hiltRule = HiltAndroidRule(this)
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @Inject
+    lateinit var repository: FutterAppRepository
+
+    private lateinit var viewModel: RatingViewModel
+
+    @Before
+    fun setup() {
+        hiltRule.inject()
+        viewModel = RatingViewModel(repository)
+    }
+
+    @Test
+    fun insertRating() {
+        viewModel.insertRating(value = 3, comment = "gut")
+        val value = viewModel.insertRatingStatus.getOrAwaitValueTest()
+        assertThat(value.getContentIfNotHandled()?.status).isEqualTo(Status.SUCCESS)
+    }
+}
---
+++ app/src/main/java/de/tierwohlteam/android/futterapp/viewModels/RatingViewModel.kt
@@ -0,0 +1,36 @@
+package de.tierwohlteam.android.futterapp.viewModels
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import de.tierwohlteam.android.futterapp.models.Rating
+import de.tierwohlteam.android.futterapp.others.Event
+import de.tierwohlteam.android.futterapp.others.Resource
+import de.tierwohlteam.android.futterapp.repositories.FutterAppRepository
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@ExperimentalCoroutinesApi
+@HiltViewModel
+class RatingViewModel @Inject constructor(
+    private val repository: FutterAppRepository,
+) : ViewModel(){
+
+    private val _insertRatingStatus = MutableLiveData<Event<Resource<Rating>>>(Event(Resource.empty()))
+    val insertRatingStatus: LiveData<Event<Resource<Rating>>> = _insertRatingStatus
+
+    fun insertRating(value: Int, comment: String) {
+        if(value < 0) {
+            _insertRatingStatus.postValue(Event(Resource.error("Rating has to be positive or null", null)))
+            return
+        }
+        val rating = Rating(value = value, comment =  comment)
+        viewModelScope.launch {
+            repository.insertRating(rating)
+        }
+        _insertRatingStatus.postValue(Event(Resource.success(rating)))
+    }
+}

174e85b49f0671c2171b04834afb22002ba43e47
Joerg-Schultz <JoergSky@online.de>
2021-10-09 08:35:33 +0200

    added fragment skeletons

---
+++ app/src/main/java/de/tierwohlteam/android/futterapp/fragments/FridgeFragment.kt
@@ -0,0 +1,7 @@
+package de.tierwohlteam.android.futterapp.fragments
+
+import androidx.fragment.app.Fragment
+import de.tierwohlteam.android.futterapp.R
+
+class FridgeFragment: Fragment(R.layout.fridge_fragment) {
+}
---
+++ app/src/main/java/de/tierwohlteam/android/futterapp/fragments/RatingFragment.kt
@@ -0,0 +1,7 @@
+package de.tierwohlteam.android.futterapp.fragments
+
+import androidx.fragment.app.Fragment
+import de.tierwohlteam.android.futterapp.R
+
+class RatingFragment: Fragment(R.layout.rating_fragment) {
+}

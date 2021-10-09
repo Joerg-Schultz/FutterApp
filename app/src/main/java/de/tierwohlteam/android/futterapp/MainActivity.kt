562dd5295da8c149d751ba9497500b604688aff3
Joerg-Schultz <JoergSky@online.de>
2021-10-09 09:04:43 +0200

    bottomnavigation working

--- app/src/main/java/de/tierwohlteam/android/futterapp/MainActivity.kt
+++ app/src/main/java/de/tierwohlteam/android/futterapp/MainActivity.kt
@@ -2,19 +2,30 @@ package de.tierwohlteam.android.futterapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import dagger.hilt.android.AndroidEntryPoint
+import de.tierwohlteam.android.futterapp.databinding.MainActivityBinding
 import de.tierwohlteam.android.futterapp.fragments.MainFragment
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: MainActivityBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
-        if (savedInstanceState == null) {
+        binding = MainActivityBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+/*        if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, MainFragment.newInstance())
                 .commitNow()
         }
+ */
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bottomNavigationView.setupWithNavController(navController)
     }
 }

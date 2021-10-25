a2aaab3fbd0b4a97b6e9fb29f6863c3b45e7c126
Joerg-Schultz <JoergSky@online.de>
2021-10-25 12:49:22 +0200

    Splash Image + png logos for icons

--- app/src/main/java/de/tierwohlteam/android/futterapp/MainActivity.kt
+++ app/src/main/java/de/tierwohlteam/android/futterapp/MainActivity.kt
@@ -14,6 +14,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: MainActivityBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.Theme_FutterApp)
+        //setContentView(R.layout.main_activity)
         binding = MainActivityBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)

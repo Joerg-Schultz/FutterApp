c903f5fe05b6bd49429efe1d6c6fc73fbbc60c25
Joerg-Schultz <JoergSky@online.de>
2021-10-29 12:14:21 +0200

    Preparation for Fragment Tests

---
+++ app/src/debug/java/de/tierwohlteam/android/futterapp/HiltTestActivity.kt
@@ -0,0 +1,7 @@
+package de.tierwohlteam.android.futterapp
+
+import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class HiltTestActivity : AppCompatActivity()
